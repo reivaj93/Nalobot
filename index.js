965ad929afdeb1e9a4297d444ae1733962af301c
reivaj93 <jav.alonso0493@gmail.com>
2024-08-15 12:39:48 -0600

    First Version

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require ('express')
+var ejs = require ('ejs')
+
+var app = express ();
+
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.listen(8081);
+
+app.get('/',function(req,res){
+res.render('pages/index');
+});
